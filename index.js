4fa1447e73410fd1794c291a7a34bb7609a2f972
sakshi <sakshimall1828@gmail.com>
2023-08-15 16:02:29 +0530

    SEVENTH COMMIT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ submitContactDetails.addEventListener("submit", (e) => {
   let selectedService = document.getElementById("selectedService").value;
   let message = document.getElementById("message").value;
   let finalMessage = `Hello Dehah Industries, %0a I'm reaching out to you to discuss about service for ${selectedService} sector.
-    %0a %0a Name: ${name} %0a Email Address: ${email}  %0a Message: ${message} %0a %0a Thank you!`;
+    %0a %0a Name: ${name} %0a Email Address: ${email}  %0a Message:${message} %0a %0a Thank you!`;
   if (name !== "" && email !== "" && message !== "" && selectedService !== "") {
     window.open(`https://wa.me/+918941026113/?text=${finalMessage}`, "_blank");
     document.getElementById("name").value = "";
